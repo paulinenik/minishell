a5765d48435f9f384d49832e591b082ea8c5c411
Raguel Gordon <rgordon@student.21-school.ru>
2021-04-06 15:42:41 +0300

    2

--- mshell.h
+++ mshell.h
@@ -6,7 +6,7 @@
 /*   By: rgordon <rgordon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 19:35:10 by rgordon           #+#    #+#             */
-/*   Updated: 2021/04/05 23:17:08 by rgordon          ###   ########.fr       */
+/*   Updated: 2021/04/06 15:42:29 by rgordon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 #include <stdlib.h>
 #include "libft.h"
 
+##dgfdgfdg
 typedef struct	s_args
 {
 	char	*bin;
 	char    **args;
 }				t_args;
-
 typedef struct	s_data
 {
 	t_args			*arg;
