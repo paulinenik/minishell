1128227d8c519f1d68ecd7a36be7edf011217be3
Raguel Gordon <rgordon@student.21-school.ru>
2021-04-09 18:34:44 +0300

    fixed pwdpwd type error

--- get_pwd.c
+++ get_pwd.c
@@ -4,7 +4,7 @@
 void    get_pwd(t_data *data)
 {
     char dir[66];
-    if (!ft_strncmp(data->bin, "pwd", 3))
+    if (!ft_strncmp(data->bin, "pwd\0", 4))
     {
         getcwd(dir, 66);
         printf("%s\n", dir);
