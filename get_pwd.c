436a5e2b4c8306f6e7b2682247369443d949e270
Jamila Shondra <jshondra@ev-c1.kzn.21-school.ru>
2021-04-12 17:59:57 +0300

    rob-sort

--- get_pwd.c
+++ get_pwd.c
@@ -19,12 +19,16 @@ void    get_pwd(t_data *data)
 
 char	**alph_sort(char **copy, int n)
 {
-		int l;
-		for(int i = 0 ; i < n; i++)
+		int j;
+		int i;
+
+		i = -1;
+		while(++i < n)
 		{
 			printf("%s\n", copy[i]);
 			printf("hellwo\n");
-        	for(int j = i + 1; j < n; j++)
+			j = i + 1;
+        	while(j < n)
 			{
 				printf("hellwo\n");
 				if(ft_strcmp(copy[i], copy[j]) > 0)
@@ -33,7 +37,7 @@ char	**alph_sort(char **copy, int n)
 					copy[i] = copy[j];
 					copy[j] = tmp;
 				}
-    
+				j++;
 			}
 		}
 		return(copy);
